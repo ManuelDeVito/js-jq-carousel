57c4be3bc8f913ea9c86fb99cf9edced4f343ecb
Manuel <manuel.devito@icloud.com>
2020-10-27 17:39:51 +0100

    parte seconda

--- main.js
+++ main.js
@@ -5,16 +5,38 @@
 $(document).ready(function() {
 
     $('.next i').click(function() {
-        alert('ciao');
 
-        // var img_corrente = $('img.active');
-        //
-        // img_corrente.removeClass('active');
-        //
-        // if (img_corrente.next().length) {
-        //     img_corrente.next()addClass('active')
-        // } else {
-        //     $('.slides :first-child').addClass('active');
-        // }
+        var img_corrente = $('img.active');
+
+        img_corrente.removeClass('active');
+
+        if (img_corrente.next('img').length) {
+            img_corrente.next('img').addClass('active');
+
+        } else {
+            $('.slides :first-child').addClass('active');
+        }
+
+        var circles_corrente = $('i.active')
+        circles_corrente.removeClass('active');
+
+        if (circles_corrente.next('i').length) {
+            circles_corrente.next('i').addClass('active');
+        } else {
+            $('.circles :first-child').addClass('active');
+        }
+    });
+
+    $('.prev i').click(function() {
+
+        var img_corrente = $('img.active');
+
+        img_corrente.removeClass('active');
+
+        if (img_corrente.prev('img').length) {
+            img_corrente.prev('img').addClass('active')
+        } else {
+            $('.slides img:last-of-type').addClass('active');
+        }
     });
 });
